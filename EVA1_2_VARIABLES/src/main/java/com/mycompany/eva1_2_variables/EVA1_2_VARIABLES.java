39b4d41e688c0cdb391a236c0d08906cec301b85
obed-27 <l24550299@chihuahua2.tecnm.mx>
2024-08-27 15:13:23 -0700

    Add files via upload

---
+++ EVA1_2_VARIABLES/src/main/java/com/mycompany/eva1_2_variables/EVA1_2_VARIABLES.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_2_variables;
+
+/**
+ *
+ * @author obeds
+ */
+public class EVA1_2_VARIABLES {
+
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO!!");//cadena de texto ("")
+        System.out.println(2);//numero entero
+        System.out.println('q');//caracter('')
+        System.out.println(true);//valor logico 
+        System.out.println(false);//valor logico
+        System.out.println(1000.10);//numero con fraccion
+        //tipose de datos 
+        //enteros 
+        //int
+        //long
+        //byte
+        //short
+        //puntos flotantes
+        //double 
+        //float
+        //que tipos de datos maneja java 
+        //string 
+        //caracter
+        //chat
+        //logicos (true, false)
+        //bolean
+        //creacion y uso de variables 
+        //declaracion(tipo dde dato: de la variable, que operacion debe hacer o puede hacer,cuanta memoria va a usar)
+        int edad;//declaro una variable de tipo entero, llamada edad
+        //double edad; (no podemos tenewr dos variables con el mismo nommbre
+        String nombre;
+        String apellidos;
+        double promedio;
+        //asignacion(poner un valor en la variable 
+        edad = 18; //asignar un valor (asignamos 18 a la variable edad
+        System.out.println(edad);
+    }
+}
