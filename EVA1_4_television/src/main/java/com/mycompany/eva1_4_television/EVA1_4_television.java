6e7b2414a082a097e9974e98f126c107fe851b7a
obed-27 <l24550299@chihuahua2.tecnm.mx>
2024-09-02 19:32:40 -0700

    Add files via upload

---
+++ EVA1_4_television/src/main/java/com/mycompany/eva1_4_television/EVA1_4_television.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_4_television;
+
+/**
+ *
+ * @author obeds
+ */
+public class EVA1_4_television {
+
+    public static void main(String[] args) {
+        String marca;
+        String modelo;
+        int pulgadas;
+        double precio;
+        String tecn;
+        marca="LG";
+        modelo="UHD UR78";
+        pulgadas=43;
+        precio=6499;
+        tecn="led";
+        System.out.println(marca);
+        System.out.println(pulgadas);
+        System.out.println(precio);
+        System.out.println(modelo);
+        System.out.println(tecn);
+    }
+}
