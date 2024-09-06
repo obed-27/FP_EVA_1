d0b51884bfecdef00e189ad5df2d5a933bf97540
obed-27 <l24550299@chihuahua2.tecnm.mx>
2024-09-05 19:25:13 -0700

    Add files via upload

---
+++ EVA1_6_PELICULA/src/main/java/com/mycompany/eva1_6_pelicula/EVA1_6_PELICULA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_6_pelicula;
+
+/**
+ *
+ * @author obeds
+ */
+public class EVA1_6_PELICULA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
