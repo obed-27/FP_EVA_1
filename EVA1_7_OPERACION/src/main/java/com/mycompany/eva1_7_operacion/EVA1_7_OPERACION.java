b099aee3e9681d276b8d17d5ffcc5bd4adca7231
obed-27 <l24550299@chihuahua2.tecnm.mx>
2024-09-05 19:26:47 -0700

    Add files via upload

---
+++ EVA1_7_OPERACION/src/main/java/com/mycompany/eva1_7_operacion/EVA1_7_OPERACION.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_7_operacion;
+
+/**
+ *
+ * @author obeds
+ */
+public class EVA1_7_OPERACION {
+
+    public static void main(String[] args) {
+       int x = 10;
+       int y = 5;
+       int suma, resta, division, mu;
+       suma = x + y;
+       System.out.println(suma);
+       resta = x - y;
+       System.out.println(resta);
+       division = x / y;
+       System.out.println(division);
+       mu = x * y;
+       System.out.println(mu);
+       
+       x = 5;
+       y = 2;
+       division = x/y;
+       System.out.println(division);
+       double v1 = 11,v2 = 3;
+       double div;
+       div = v1/v2;
+       System.out.println(div);
+    }
+}
