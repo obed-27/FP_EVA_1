116ed89649fb6fb1a96754271f376a984b50e5fc
obed-27 <l24550299@chihuahua2.tecnm.mx>
2024-09-05 19:27:26 -0700

    Add files via upload

---
+++ EVA1_8_OPERACIONES/src/main/java/com/mycompany/eva1_8_operaciones/EVA1_8_OPERACIONES.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_8_operaciones;
+
+/**
+ *
+ * @author obeds
+ */
+public class EVA1_8_OPERACIONES {
+
+    public static void main(String[] args) {
+        String NOMBRE = "Obed";
+        String APELLIDO ="FIMBRES";
+        String nomCom;
+        nomCom = NOMBRE + " " + APELLIDO;
+        System.out.println("EL NOMBRE ES: "+NOMBRE);
+        System.out.println("EL APELLIDO ES: "+APELLIDO);
+        System.out.println(nomCom);
+    }
+}
